f2eaec236f07b95eacacf0f12ccbdc2069959691
Augnik <shresnik2004@gmail.com>
2024-04-04 23:31:22 +0530

    updated styling

--- src/components/SessionCounter.jsx
+++ src/components/SessionCounter.jsx
@@ -12,12 +12,12 @@ const SessionCounter = () => {
       <div className="bg-white p-4 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold mb-2">Sessions Completed</h2>
         <div className="flex items-center">
-          <span className="text-4xl font-bold mr-2">{sessionsCompleted}</span>
+          <span className="text-4xl font-bold mr-2 ml-16">{sessionsCompleted}</span>
           <button
-            className="bg-green-600 text-white rounded-lg px-4 py-2 hover:bg-green-500 transition-colors duration-300"
+            className="bg-green-600 text-white rounded-lg px-4 py-2 hover:bg-green-500 transition-colors duration-300 "
             onClick={handleCompleteSession}
           >
-            Completed Sessions
+            +
           </button>
         </div>
       </div>
